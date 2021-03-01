2b301224fb46e4104f6a2eca6bc6fc5a2e8832b8
yjsdl <yj160521@163.com>
2021-03-02 00:21:17 +0800

    进程

---
+++ 进程/02-通过queue来实现数据共享.py
@@ -0,0 +1,38 @@
+import multiprocessing
+
+
+def download_from_web(q):
+    """下载数据"""
+    # 模拟从网上下载的数据
+    data = [11,22,33,44]
+
+    # 向队列中写入数据
+    for temp in data:
+        q.put(temp)
+    print("数据下载完成并且存入到队列中")
+def analysis_data(q):
+    """数据处理"""
+    waitting_analysis_data = list()
+    # 从队列中获取数据
+    while True:
+        data = q.get()
+        waitting_analysis_data.append(data)
+        if q.empty():
+            break
+    
+    # 模拟数据处理
+    print(waitting_analysis_data)
+
+def main():
+    # 1.创建一个队列
+    q = multiprocessing.Queue()
+    p1 = multiprocessing.Process(target=download_from_web,args=(q,))
+    p2 = multiprocessing.Process(target=analysis_data, args=(q,))
+    p1.start()
+    p2.start()
+
+
+
+
+if __name__ == "__main__":
+        main()
